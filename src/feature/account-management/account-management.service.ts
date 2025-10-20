d88affa67c5fd4987b8812b71b12991b995826af
AghnatHs <aghnathasya@gmail.com>
2025-10-20 16:17:19 +0700

    Feat: ITransactionalService to provide transaction capability

--- src/feature/account-management/account-management.service.ts
+++ src/feature/account-management/account-management.service.ts
@@ -1,23 +1,31 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { DataSource, EntityManager, Repository } from 'typeorm';
 import { Parent } from '../users/entities/parent.entity';
 import { Student } from '../users/entities/student.entity';
 import { Teacher } from '../users/entities/teacher.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TokenGeneratorService } from 'src/common/token-generator/token-generator.service';
 import * as bcrypt from 'bcrypt';
+import { ITransactionalService } from 'src/common/base-transaction/transactional.interface.service';
 
 @Injectable()
-export class AccountManagementService {
+export class AccountManagementService extends ITransactionalService {
   constructor(
+    dataSource: DataSource,
+
     @InjectRepository(Parent)
     private readonly parentRepository: Repository<Parent>,
+
     @InjectRepository(Teacher)
     private readonly teacherRepository: Repository<Teacher>,
+
     @InjectRepository(Student)
     private readonly studentRepository: Repository<Student>,
+
     private readonly tokenGeneratorService: TokenGeneratorService,
-  ) {}
+  ) {
+    super(dataSource);
+  }
 
   public async createStudentWithParentAccount(
     studentUsername: string,
@@ -26,66 +34,73 @@ export class AccountManagementService {
     parentFullName: string,
     teacherId: string,
   ): Promise<Student> {
-    const existingStudent: Student | null =
-      await this.studentRepository.findOne({
+    return this.withTransaction<Student>(async (manager: EntityManager) => {
+      const studentRepo: Repository<Student> = manager.getRepository(Student);
+      const parentRepo: Repository<Parent> = manager.getRepository(Parent);
+      const teacherRepo: Repository<Teacher> = manager.getRepository(Teacher);
+
+      const existingStudent: Student | null = await studentRepo.findOne({
         where: {
           username: studentUsername,
         },
         relations: ['teacher', 'parent'],
       });
 
-    if (existingStudent) {
-      throw new BadRequestException(
-        `Student dengan username ${studentUsername} sudah pernah terdaftar`,
-      );
-    }
+      if (existingStudent) {
+        throw new BadRequestException(
+          `Student dengan username ${studentUsername} sudah pernah terdaftar`,
+        );
+      }
 
-    let parent: Parent | null = await this.parentRepository.findOne({
-      where: {
-        email: parentEmail,
-      },
-      relations: ['students'],
-    });
-
-    if (!parent) {
-      const parentUsername: string = parentEmail.split('@')[0];
-      const parentPassowrd: string = this.tokenGeneratorService.numericCode(6);
-      const hashedPassword: string = await bcrypt.hash(parentPassowrd, 10);
-      parent = this.parentRepository.create({
-        id: this.tokenGeneratorService.randomUUIDV7(),
-        username: parentUsername,
-        email: parentEmail,
-        password: hashedPassword,
-        fullName: parentFullName,
+      const teacher: Teacher | null = await teacherRepo.findOneBy({
+        id: teacherId,
       });
-      await this.parentRepository.save(parent);
-    }
+      if (!teacher) {
+        throw new BadRequestException(
+          `Teacher dengan ID ${teacherId} tidak ditemukan`,
+        );
+      }
 
-    const studentPassword: string = this.tokenGeneratorService.numericCode(6);
-    const hashedStudentPassword: string = await bcrypt.hash(
-      studentPassword,
-      10,
-    );
-    const student: Student = this.studentRepository.create({
-      id: this.tokenGeneratorService.randomUUIDV7(),
-      username: studentUsername,
-      password: hashedStudentPassword,
-      fullName: studentFullName,
-    });
-    student.parent = parent!; // parent is guaranteed to exist here
+      let parent: Parent | null = await parentRepo.findOne({
+        where: {
+          email: parentEmail,
+        },
+        relations: ['students'],
+      });
 
-    const teacher: Teacher | null = await this.teacherRepository.findOneBy({
-      id: teacherId,
-    });
-    if (!teacher) {
-      throw new BadRequestException(
-        `Teacher dengan ID ${teacherId} tidak ditemukan`,
+      if (!parent) {
+        const parentUsername: string = parentEmail.split('@')[0];
+        const parentPassowrd: string =
+          this.tokenGeneratorService.numericCode(6);
+        const hashedPassword: string = await bcrypt.hash(parentPassowrd, 10);
+        parent = parentRepo.create({
+          id: this.tokenGeneratorService.randomUUIDV7(),
+          username: parentUsername,
+          email: parentEmail,
+          password: hashedPassword,
+          fullName: parentFullName,
+        });
+        await parentRepo.save(parent);
+      }
+
+      const studentPassword: string = this.tokenGeneratorService.numericCode(6);
+      const hashedStudentPassword: string = await bcrypt.hash(
+        studentPassword,
+        10,
       );
-    }
-    student.teacher = teacher;
+      const student: Student = studentRepo.create({
+        id: this.tokenGeneratorService.randomUUIDV7(),
+        username: studentUsername,
+        password: hashedStudentPassword,
+        fullName: studentFullName,
+      });
+      student.parent = parent!; // parent is guaranteed to exist here
 
-    const savedStudent: Student = await this.studentRepository.save(student);
+      student.teacher = teacher;
 
-    return savedStudent;
+      const savedStudent: Student = await studentRepo.save(student);
+
+      return savedStudent;
+    });
   }
 }
--- src/feature/users/teacher/teacher.module.ts
+++ src/feature/users/teacher/teacher.module.ts
@@ -6,10 +6,13 @@ import { TeacherService } from './teacher.service';
 import { TokenGeneratorModule } from 'src/common/token-generator/token-generator.module';
 import { AuthModule } from 'src/feature/auth/auth.module';
 import { AccountManagementModule } from 'src/feature/account-management/account-management.module';
+import { BaseTransactionModule } from 'src/common/base-transaction/base-transaction.module';
 
 @Module({
   imports: [
     AccountManagementModule,
+
+    BaseTransactionModule,
 
     TypeOrmModule.forFeature([Teacher]),
 
