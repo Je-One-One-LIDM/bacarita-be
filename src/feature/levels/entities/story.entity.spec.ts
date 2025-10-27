9c98f95be267d355eb777e86e1f6ff7f49b74685
AghnatHs <aghnathasya@gmail.com>
2025-10-28 00:04:14 +0700

    Unit Test(Story): check validity of student to start a test session using the story

--- src/feature/levels/entities/story.entity.spec.ts
+++ src/feature/levels/entities/story.entity.spec.ts
@@ -1,5 +1,8 @@
 import { Story } from 'src/feature/levels/entities/story.entity';
 import { StoryStatus } from 'src/feature/levels/enum/story-status.enum';
+import { Student } from 'src/feature/users/entities/student.entity';
+import { Level } from './level.entity';
+import { LevelProgress } from './level-progress.entity';
 
 describe('Unit Test: Story Entity', () => {
   it('must create a story with default status', () => {
@@ -38,5 +41,32 @@ describe('Unit Test: Story Entity', () => {
       'Fourth sentence',
       'fifth sentence',
     ]);
+  });
+
+  it('(isCurrentStudentValidForStory) must handle correctly', () => {
+    const student: Student = new Student();
+    student.id = 'student-1';
+
+    const level1: Level = new Level();
+    const level2: Level = new Level();
+    const levelProgress1 = new LevelProgress();
+    levelProgress1.student = student;
+    levelProgress1.level = level1;
+    levelProgress1.isUnlocked = true;
+
+    const levelProgress2 = new LevelProgress();
+    levelProgress2.student = student;
+    levelProgress2.level = level2;
+    levelProgress2.isUnlocked = false;
+
+    const story1: Story = new Story();
+    story1.level = level1;
+    level1.levelProgresses = [levelProgress1];
+    const story2: Story = new Story();
+    story2.level = level2;
+    level2.levelProgresses = [levelProgress2];
+
+    expect(story1.isCurrentStudentValidForStory('student-1')).toBe(true);
+    expect(story2.isCurrentStudentValidForStory('student-1')).toBe(false);
   });
 });
--- src/feature/levels/entities/story.entity.ts
+++ src/feature/levels/entities/story.entity.ts
@@ -1,15 +1,18 @@
+import { Expose } from 'class-transformer';
+import { TestSession } from 'src/feature/test-session/entities/test-session.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { StoryStatus } from '../enum/story-status.enum';
 import { Level } from './level.entity';
-import { Expose } from 'class-transformer';
+import { LevelProgress } from './level-progress.entity';
 
 @Entity('stories')
 export class Story {
@@ -39,6 +42,12 @@ export class Story {
   })
   status: StoryStatus = StoryStatus.WAITING_NEWLY;
 
+  @OneToMany(
+    () => TestSession,
+    (testSession: TestSession) => testSession.student,
+  )
+  testSessions: TestSession[];
+
   @CreateDateColumn()
   createdAt: Date;
 
@@ -56,6 +65,15 @@ export class Story {
   @Expose()
   get passageSentences(): string[] {
     return Story.passageToSentences(this.passage);
+  }
+
+  @Expose()
+  public isCurrentStudentValidForStory(studentId: string): boolean {
+    return (
+      this.level?.levelProgresses?.some(
+        (lp: LevelProgress) => lp.student.id === studentId && lp.isUnlocked,
+      ) ?? false
+    );
   }
 
   public static passageToSentences(passage: string): string[] {
